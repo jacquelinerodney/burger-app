aab0bc05acba218e7796a0b9e2f19f92538f5de3
Jacqueline Rodney <jacquelinerodney7@gmail.com>
2021-04-07 11:47:18 -0400

    express handlebars

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-const require = express
+const require = require ("express");
+const exphbs = require ("express-handlebars");
+const app = express ();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+//parse application json
+app.use(express.urlencoded({extended: true }));
+app.use(express.json());
+
+//handlebars
+app.engine('handlebars', exphbs ({defaultLayout: 'main'}));
+app.set('vew engine', 'handlebars');
+
+
+//routes
+const route = require('.contollers/burgerController.js');
+app.use (routes);
+
+//listening on port
+app.listen(PORT, () =>
+    console.log(`Server listening on port ${PORT}`)
+
+);
