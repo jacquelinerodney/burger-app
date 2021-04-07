e900097d208641a4cf6cec44f5c39feeb46b3aff
Jacqueline Rodney <jacquelinerodney7@gmail.com>
2021-04-07 12:09:17 -0400

    seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('Cheese Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Shrimp Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Bacon Burger', false);
