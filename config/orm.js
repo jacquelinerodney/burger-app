ce6e607a278887c48784abae5b99a2089fe51a39
Jacqueline Rodney <jacquelinerodney7@gmail.com>
2021-04-10 02:05:03 -0400

    connection query

--- config/orm.js
+++ config/orm.js
@@ -13,7 +13,34 @@ const orm= {
 
 //deleteone
 
-
+selectAll(cb) {
+    const query = "SELECT * FROM burgers;";
+    connection.query(query, (err, res) => {
+      if (err) throw err;
+      cb(res);
+    });
+  },
+  insertOne(value, cb) {
+    const query = "INSERT INTO burgers (burgerName) VALUES (?);";
+    connection.query(query, [value], (err, res) => {
+      if (err) throw err;
+      cb(res);
+    });
+  },
+  updateOne(value, cb) {
+    const query = "UPDATE burgers SET devoured = true WHERE id = ?";
+    connection.query(query, [value], (err, res) => {
+      if (err) throw err;
+      cb(res);
+    });
+  },
+  deleteOne(value, cb) {
+    const query = "DELETE FROM burgers WHERE id = ?";
+    connection.query(query, [value], (err, res) => {
+      if (err) throw err;
+      cb(res);
+    });
+  },
 
 
 };
